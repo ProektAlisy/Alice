b5bf215f48450cf0c36b61c9a35eafa93c1d8ff4
Каманин Юрий <kamanchi22@mail.ru>
2024-01-13 16:14:04 +0300

    Первая болванка. Структура вот такая. Расширяем добавляем.

---
+++ machine.py
@@ -0,0 +1,60 @@
+from transitions import Machine
+import logging
+
+from constants import TRANSITIONS
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class Skills(object):
+    states = [
+        "start",
+        "info_center",
+        "info_personal",
+        "help",
+    ]
+
+    transitions = TRANSITIONS
+
+    def __init__(self, name):
+        self.name = name
+        self.data = {}
+        self.saved_state = None
+        self.machine = Machine(
+            model=self,
+            states=Skills.states,
+            transitions=Skills.transitions,
+            initial="start",
+        )
+
+    def _save_state(self):
+        self.saved_state = self.state
+
+    def _return_to_original_state(self):
+        self.machine.set_state(self.saved_state)
+
+    def get_info_about_center(self):
+        self.data = {"message": "Наш центр самый самый и все такое"}
+
+    def get_info_about_center_personal(self):
+        self.data = {"message": "Вот наш персонал: Маша, Витя и Леонид"}
+
+    def get_info(self):
+        self.data = {"message": "Дополнительная информация"}
+
+    def get_help(self):
+        self.saved_state = self.state
+        self.data = {
+            "message": "Здесь вы можете узнать фразы взаимодействия или "
+            "получить помощь с навигацией по навыку!"
+        }
+
+    def get_help_phrase(self):
+        self.data = {"message": "Вот это фразы взаимодействия с навыком"}
+
+    def get_help_navigation(self):
+        self.data = {"message": "Вот помощь по навигации по навыку"}
+
+    def get_help_exit(self):
+        self.data = {"message": "Вы вышли из помощи"}
---
+++ main.py
@@ -0,0 +1,105 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transitions import MachineError
+
+from machine import Skills
+from icecream import ic
+
+
+class RequestData(BaseModel):
+    request: dict
+
+
+app = FastAPI()
+
+skill = Skills("Собака-поводырь")
+
+
+class Command:
+    def execute(self, skill: Skills) -> str:
+        raise NotImplementedError
+
+
+class InfoCenterCommand(Command):
+    def execute(self, skill: Skills) -> str:
+        try:
+            skill.say_info_about_center()
+        except MachineError:
+            ic("Отсюда нельзя вызвать эту команду")
+            skill.data["message"] = "Отсюда нельзя вызвать эту команду"
+        return skill.data["message"]
+
+
+class InfoPersonalCommand(Command):
+    def execute(self, skill: Skills) -> str:
+        skill.say_info_about_center_personal()
+        return skill.data["message"]
+
+
+class AdditionalInfo(Command):
+    def execute(self, skill: Skills) -> str:
+        if skill.state == "info_center":
+            skill.say_info()
+            return skill.data["message"]
+        else:
+            return "Эта команда доступна только в состоянии info_center"
+
+
+class HelpCommand(Command):
+
+    def execute(self, skill: Skills) -> str:
+        skill.say_help()
+        return skill.data["message"]
+
+
+class HelpPhraseCommand(Command):
+    def execute(self, skill: Skills) -> str:
+        skill.say_help_phrase()
+        return skill.data["message"]
+
+
+class HelpNavigationCommand(Command):
+    def execute(self, skill: Skills) -> str:
+        skill.say_help_navigation()
+        return skill.data["message"]
+
+
+class HelpExitCommand(Command):
+
+    def execute(self, skill: Skills) -> str:
+        skill.from_help()
+        return skill.data["message"]
+
+
+command_mapping = {
+    "расскажи о центре": InfoCenterCommand,
+    "расскажи о персонале": InfoPersonalCommand,
+    "дополнительная информация": AdditionalInfo,
+    "помощь": HelpCommand,
+    "хочу узнать фразы": HelpPhraseCommand,
+    "помоги с навигацией": HelpNavigationCommand,
+    "выход": HelpExitCommand,
+}
+
+
+@app.post("/")
+async def root(data: RequestData):
+    command = data.request.get("command")
+    command_class = command_mapping.get(command.lower())
+
+    if command_class:
+        command_instance = command_class()
+        answer = command_instance.execute(skill)
+    elif command.lower() == "":
+        answer = "Приветствую!"
+    else:
+        answer = "Я не понимаю, что вы хотите сказать"
+    ic(command, skill.state)
+    response = {
+        "response": {
+            "text": answer,
+            "end_session": False,
+        },
+        "version": "1.0",
+    }
+    return response
