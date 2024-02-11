54f3fe2bae7302b1bdc8d12e3805fa0f04456173
Igor <borny@bk.ru>
2024-02-11 21:43:09 +0700

    flake8 fix

--- app/machine.py
+++ app/machine.py
@@ -119,10 +119,10 @@ class FiniteStateMachine:
 
             if self.is_agree():
                 after_answer = get_after_answer_by_trigger(
-                    trigger, COMMANDS_TRIGGERS_GET_FUNC_ANSWERS
+                    trigger, COMMANDS_TRIGGERS_GET_FUNC_ANSWERS,
                 )
                 answer = get_answer_by_trigger(
-                    trigger, COMMANDS_TRIGGERS_GET_FUNC_ANSWERS
+                    trigger, COMMANDS_TRIGGERS_GET_FUNC_ANSWERS,
                 )
             else:
                 if (
@@ -130,12 +130,12 @@ class FiniteStateMachine:
                     and self.previous_command == "нет"
                 ):
                     answer = disagree_answer_by_trigger(
-                        trigger, COMMANDS_TRIGGERS_GET_FUNC_ANSWERS
+                        trigger, COMMANDS_TRIGGERS_GET_FUNC_ANSWERS,
                     )
                     after_answer = ""
                 else:
                     answer = get_answer_by_trigger(
-                        trigger, COMMANDS_TRIGGERS_GET_FUNC_ANSWERS
+                        trigger, COMMANDS_TRIGGERS_GET_FUNC_ANSWERS,
                     )
                     after_answer = self.get_next_after_answer(trigger)
 
