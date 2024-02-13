9a82a1f17b3cd8f71521225b3751ec5ae5d4a0da
Igor <borny@bk.ru>
2024-02-13 11:42:29 +0700

    Mongo set up

--- app/monga_initialize.py
+++ app/monga_initialize.py
@@ -6,11 +6,11 @@ from pymongo import MongoClient
 load_dotenv()
 
 
-mongo_test_host = os.getenv("ME_CONFIG_MONGODB_URL")
+mongo_host = os.getenv("ME_CONFIG_MONGODB_URL")
 mongo_user = os.getenv("MONGO_INITDB_ROOT_USERNAME")
 mongo_pass = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
 
-client = MongoClient(host=mongo_test_host)
+client = MongoClient(host=mongo_host)
 db = client["database"]
 answers_collection = db["answers"]
 after_answers_collection = db["after_answers"]
