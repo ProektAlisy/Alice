40b05c158a016f61ffc88e06fdacc5bbea591592
Igor <borny@bk.ru>
2024-02-14 14:08:03 +0700

    mongo init fix

--- app/monga_initialize.py
+++ app/monga_initialize.py
@@ -1,16 +1,16 @@
-import os
+# import os
 
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv()
 
-mongo_host = os.getenv("MONGO_TEST_HOST")
-mongo_user = os.getenv("MONGO_TEST_USER")
-mongo_pass = os.getenv("MONGO_TEST_PASSWORD")
-mongo_port = os.getenv("MONGO_PORT")
+# mongo_host = os.getenv("MONGO_TEST_HOST")
+# mongo_user = os.getenv("MONGO_TEST_USER")
+# mongo_pass = os.getenv("MONGO_TEST_PASSWORD")
+# mongo_port = os.getenv("MONGO_PORT")
 
-uri = f"mongodb://{mongo_user}:{mongo_pass}@{mongo_host}:{mongo_port}/"
+uri = "mongodb://root:example@www.guidedogs.acceleratorpracticum.ru:27017/"
 
 
 client = MongoClient(uri)
