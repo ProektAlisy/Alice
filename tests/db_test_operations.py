1bb3d8f1df4b763b2b17c0ba6b51aae81714fedb
Igor <borny@bk.ru>
2024-02-15 15:23:28 +0700

    pytest fix and return auto test in GitHub Actions

--- tests/db_test_operations.py
+++ tests/db_test_operations.py
@@ -1,25 +1,16 @@
 import os
 
 from dotenv import load_dotenv
-from pymongo import MongoClient
+from mongomock import MongoClient
 
 load_dotenv()
 
 mongo_test_host = os.getenv("MONGO_TEST_HOST")
 mongo_user = os.getenv("MONGO_TEST_USER")
 mongo_pass = os.getenv("MONGO_TEST_PASSWORD")
-mongo_port = os.getenv("MONGO_PORT")
 
-# uri = ("mongodb://adminUser:adminPassword@"
-#        "www.guidedogs.acceleratorpracticum.ru"
-#        ":27017/?authSource=adminUser")
 
-client = MongoClient(
-    'www.guidedogs.acceleratorpracticum.ru',
-    username='adminUser',
-    password='adminPassword',
-    authMechanism='SCRAM-SHA-256',
-)
+client = MongoClient()
 db = client["test_db"]
 
 
