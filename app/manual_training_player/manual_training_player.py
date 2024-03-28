f1eb6c576fbe7f114082a8a7e3f1d40f72a8af22
Igor Evsyukov <borny@bk.ru>
2024-03-28 15:05:01 +0700

    more fixes

--- app/manual_training_player/manual_training_player.py
+++ app/manual_training_player/manual_training_player.py
@@ -194,7 +194,7 @@ class ManualTrainingPlayer:
         return self.start_audio_playback(self.current_chapter)
 
     def play_final_audio(self):
-        if int(self.current_chapter) == 13:
+        if self.current_chapter and int(self.current_chapter) == 13:
             audio_url = ("https://www.guidedogs.acceleratorpracticum.ru/"
                          "finish.mp3")
             directives = {
